7c8c18d3c0df05ae26f15c96361d2ce0604c4550
Zacky Rio Orlando <zackyrioorlando@gmail.com>
2026-03-03 16:39:50 +0700

    Laporan Hasil Praktikum 5

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        /* $exists = DB::table('m_kategori')
+            ->where('kategori_kode', 'SNK')
+            ->exists();
+
+        if (!$exists) {
+            DB::table('m_kategori')->insert([
+                'kategori_kode' => 'SNK',
+                'kategori_nama' => 'Snack/Makanan Ringan',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+
+        return 'Insert data baru berhasil'; */
+
+        /* $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris'; */
+
+        /* $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row.' baris'; */
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
